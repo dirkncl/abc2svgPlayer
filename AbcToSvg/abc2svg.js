85e316f84a3c516cb581012c84470e68dcea1719
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2020-06-28 15:26:22 +0800

    Update abc2svg.js

--- AbcToSvg/abc2svg.js
+++ AbcToSvg/abc2svg.js
@@ -14,6 +14,6 @@ tmp = fs.readFileSync('core/tune.js');abc2svg.push(tmp);tmp=null;
 tmp = fs.readFileSync('core/lyrics.js');abc2svg.push(tmp);tmp=null;
 tmp = fs.readFileSync('core/gchord.js');abc2svg.push(tmp);tmp=null;
 tmp = fs.readFileSync('core/tail.js');abc2svg.push(tmp);tmp=null;
-mod = fs.readFileSync('core/modules.js');mod=mod.replace(/\\t\\n/g,"\n");mod=mod.replace(/S+/g," ");mod = mod.replace(/[-]1/ig,"");mod = mod.replace(/fn: '/ig,"fn: \'modules/");abc2svg.push(mod);tmp=mod;
+mod = fs.readFileSync('core/modules.js');mod = mod.replace(/[-]1/ig,"");mod = mod.replace(/fn: '/ig,"fn: \'modules/");abc2svg.push(mod);tmp=mod;
 tmp = fs.readFileSync('version.js');abc2svg.push(tmp);tmp='';
 for(i=0;i<abc2svg.length;i++){tmp += abc2svg[i]}eval.apply( null, [tmp] )
