33c5a7b26536d79f1009915827dbafccc47fdc78
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2020-06-28 15:29:20 +0800

    Update abcemb.js

--- AbcToSvg/abcemb.js
+++ AbcToSvg/abcemb.js
@@ -6,5 +6,4 @@ abc_emb = abc_emb.replace("playconf = {","playconf = {sfu:sfuPath,");
 abc_emb = abc_emb.replace(/abcemb[-]/ig,"abcemb");
 abc_emb = abc_emb.replace(/[-][1]/ig,"");
 abc_emb = abc_emb.replace('window.addEventListener("load", function() {setTimeout(dom_loaded, 500)})','setTimeout(dom_loaded, 500)')
-//console.log(abc_emb);
 eval.apply(null,[abc_emb])
