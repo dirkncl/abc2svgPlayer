8aeaf8bca8bb0b1772ba9f774ce38f7a93e144c7
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2020-06-28 23:05:57 +0800

    Update abcweb.js

--- AbcToSvg/abcweb.js
+++ AbcToSvg/abcweb.js
@@ -1,9 +1,9 @@
 var abc_web;
-//var sfuPath = "../../soundfonts/Scc1t2";
+var sfuPath = "../../soundfonts/Scc1t2";
 abc_web = fs.readFileSync('util/abcweb.js');
-//abc_web = "var sfuPath = sfuPath || null;"+abc_web;
-//abc_web = abc_web.replace("playconf = {","playconf = {sfu:sfuPath,");
-//abc_web = abc_web.replace(/abcweb[-]/ig,"abcemb");
+abc_web = "var sfuPath = sfuPath || null;"+abc_web;
+abc_web = abc_web.replace("playconf = {","playconf = {sfu:sfuPath,");
+abc_web = abc_web.replace(/abcweb[-]/ig,"abcweb");
 abc_web = abc_web.replace(/[-][1]/ig,"");
 //abc_web = abc_web.replace('window.addEventListener("load", dom_loaded)','setTimeout(dom_loaded, 500)')
 //abc_web = abc_web.replace('window.addEventListener("load", dom_loaded)','');
