133ea00f75521ced35e4fe2556bfa6e8e64d77f4
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2020-07-01 10:15:05 +0800

    Update mei2svg.js

--- AbcToSvg/mei2svg.js
+++ AbcToSvg/mei2svg.js
@@ -15,6 +15,6 @@ tmp = fs.readFileSync('core/lyrics.js');meitosvg.push(tmp);tmp=null;
 tmp = fs.readFileSync('core/gchord.js');meitosvg.push(tmp);tmp=null;
 tmp = fs.readFileSync('core/mei.js');meitosvg.push(tmp);tmp=null;
 tmp = fs.readFileSync('core/tail.js');meitosvg.push(tmp);tmp=null;
-mod = fs.readFileSync('core/modules.js');mod = mod.replace(/[-]1/ig,"");mod = mod.replace(/fn: '/ig,"fn: \'modules/");meitosvg.push(mod);mod=null;
+mod = fs.readFileSync('core/modules.js');mod = mod.replace("abc2svg.modules = {","abc2svg.modules = {\ngamelan: { fn: \'gamelan-1.js\' },");mod = mod.replace(/[-]1/ig,"");mod = mod.replace(/fn: '/ig,"fn: \'modules/");meitosvg.push(mod);mod=null;
 tmp = fs.readFileSync('version.js');meitosvg.push(tmp);tmp='';
 for(i=0;i<meitosvg.length;i++){tmp += meitosvg[i]};eval.apply( null, [tmp] )
